4c21b0ea0d09de784691db8d9c69dfbab4ba9981
Awilen-Bernkastel <awilen.bernkastel@gmail.com>
2025-03-01 13:38:04 +0100

    Implement SQLStorage.save_memory_to_history

--- memoripy/sql_storage.py
+++ memoripy/sql_storage.py
@@ -8,23 +8,22 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from sqlalchemy.schema import MetaData
 
-from .sql_storage_models import MemoryOwner
+from .sql_storage_models import MemoryOwner, Memory
 from .storage import BaseStorage
 
 class SQLStorage(BaseStorage):
 
-    def __init__(self, owner="Assistant", db='sqlite://'):
+    def __init__(self, owner="Assistant", db="sqlite://", echo_sql=False):
         self.owner = None
         self.history = {
             "short_term_memory": [],
             "long_term_memory": []
         }
-        engine = create_engine(db, echo=True)
-        self.session = Session(engine)
+        self.session = Session(create_engine(db, echo=echo_sql))
 
         with self.session as s:
-            MetaData.create_all(engine)
-            stmt = select(MemoryOwner).where(MemoryOwner.name=owner)
+            MetaData.create_all(self.session)
+            stmt = select(MemoryOwner).where(MemoryOwner.name==owner)
             for memory_owner in s.scalars(stmt):
                 self.owner = memory_owner
             if self.owner is None:
@@ -39,6 +38,32 @@ class SQLStorage(BaseStorage):
             self.history["long_term_memory"]  = self.owner.memories[:last_interactions]  if len(self.owner.memories) >= last_interactions else []
         return self.history["short_term_memory"], self.history["long_term_memory"]
 
-
     def save_memory_to_history(self, memory_store):
-        raise NotImplementedError("The method save_memory_to_history() must be implemented.")
+        # Save short-term memory interactions
+        with self.session as s:
+            interaction_ids = set([x["id"] for x in self.history["short_term_memory"]])
+            for idx, (memory, embedding, timestamp, access_count, concepts) in enumerate(zip(memory_store.short_term_memory, memory_store.embeddings, memory_store.access.counts, memory_store.concepts_list)):
+                if memory["id"] not in interaction_ids:
+                    interaction = Memory()
+                    self.owner.memories.append(interaction)
+
+                    interaction.id = memory["id"]
+                    interaction.prompt = memory["prompt"]
+                    interaction.output = memory["output"]
+                    interaction.embedding = embedding.flatten().tolist()
+                    interaction.timestamp = timestamp
+                    interaction.access_count = access_count
+                    interaction.concepts = list(concepts)
+                    interaction.decay_factor = memory.get("decay_factor", 1.0)
+
+            old_interaction_ids = set([x["id"] for x in self.history["long_term_memory"]])
+            for idx, memory in enumerate(memory_store.long_term_memory):
+                if memory["id"] in old_interaction_ids:
+                    old_interaction_ids.remove(memory["id"])
+            decayed_interactions = [x for x in self.owner.memories if x.id in old_interaction_ids]
+            for interaction in decayed_interactions:
+                s.delete(interaction)
+
+            s.commit()
+
+        logging.info(f"Saved interaction history to SQL db. Short-term: {len(self.history['short_term_memory'])}, Long-term: {len(self.history['long_term_memory'])}")
