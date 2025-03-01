d866379b68b131162b2cc48cea005edb4620be2e
Awilen-Bernkastel <awilen.bernkastel@gmail.com>
2025-03-02 00:10:21 +0100

    Added SQLStorage to memoripy module

--- memoripy/__init__.py
+++ memoripy/__init__.py
@@ -2,7 +2,8 @@
 from .memory_manager import MemoryManager
 from .in_memory_storage import InMemoryStorage
 from .json_storage import JSONStorage
+from .sql_storage import SQLStorage
 from .storage import BaseStorage
 from .model import ChatModel, EmbeddingModel
 
-__all__ = ["MemoryManager", "InMemoryStorage", "JSONStorage", "BaseStorage", "ChatModel", "EmbeddingModel"]
+__all__ = ["MemoryManager", "InMemoryStorage", "JSONStorage", "BaseStorage", "ChatModel", "EmbeddingModel", "SQLStorage"]
